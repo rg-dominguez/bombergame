0327816b8dcf960f4878c47b3fc7fd1918e81105
Rodrigo García Domínguez <rogarciadomin@gmail.com>
2019-09-30 14:38:33 +0200

    modelo inicial

---
+++ cliente/modelo.js
@@ -0,0 +1,34 @@
+
+function Juego(){
+	this.partidas=[];
+	this.usuarios=[];
+
+	this.crearPartida=function(nombre, nick){
+		var idp=nombre+nick;
+		if (!this.partidas[idp]){
+			this.partidas[idp]=new Partida(nombre,idp)
+		}		
+	}
+
+	this.agregarUsuario=function(nombre){
+		if(this.usuarios[nombre]==null){
+			this.usuarios[nombre]=new Usuario(nombre);
+		}
+	}
+}
+
+
+
+function Partida(nombre,idp){
+	this.nombre=nombre;
+	this.idp=idp;
+	this.jugadores=[];
+}
+
+
+function Usuario(nick){
+	this.nick=nick;
+	this.id=undefined;
+}
+
+
